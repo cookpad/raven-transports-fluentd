6a829acc86dccf35a55c510cd37e7d97d3d508c3
Kohei Suzuki <kohei-suzuki@cookpad.com>
2014-12-01 15:27:57 +0900

    bundle gem raven-transports-fluentd

---
+++ lib/raven/transports/fluentd.rb
@@ -0,0 +1,9 @@
+require "raven/transports/fluentd/version"
+
+module Raven
+  module Transports
+    module Fluentd
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/raven/transports/fluentd/version.rb
@@ -0,0 +1,7 @@
+module Raven
+  module Transports
+    module Fluentd
+      VERSION = "0.0.1"
+    end
+  end
+end
