2fafb05a4b392679fef480bc2e7af307fc89c4aa
Takashi Kokubun <takashi-kokubun@cookpad.com>
2017-01-17 10:47:13 +0900

    Support sentry-raven >= 2.2.0
    `Raven::Transports#verify_configuration` is dropped in sentry-raven
    2.2.0.
    https://github.com/getsentry/raven-ruby/commit/fab3d6b21c875c160ba15b2858c36414c720569d
    
    Follow implementation of `Raven::Transports::HTTP`.

--- lib/raven/transports/fluentd.rb
+++ lib/raven/transports/fluentd.rb
@@ -10,25 +10,22 @@ module Raven
     class Fluentd < Transport
 
       def send_event(auth_header, data, options = {})
+        unless configuration.sending_allowed?
+          configuration.logger.debug("Event not sent: #{configuration.error_messages}")
+        end
+
         conn.post('error', auth_header: auth_header, data: data, options: options, project_id: configuration.project_id)
       end
 
       private
 
       def conn
-        verify_configuration
-
         @conn ||= begin
           uri = URI.parse(self.configuration.server)
 
           ::Fluent::Logger::FluentLogger.new('sentry', host: uri.host, port: uri.port)
         end
       end
-
-      def verify_configuration
-        super
-      end
-
     end
   end
 end
